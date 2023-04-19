5b1eda2ff5f49f746bbe815a509549ef7c62f652
Nathan G <Nathanglass26@gmail.com>
2023-04-19 22:07:21 +0100

    register and login forms

--- index.js
+++ index.js
@@ -2,16 +2,22 @@ const express = require('express');
 const app = express();
 const port = 8000;
 const middleware = require('./middleware');
+const path  = require('path');
 
 const server = app.listen(port, () => console.log('listening on port 8000'));
 
 app.set('view engine', 'pug');
 app.set('views', 'views');
 
+// direct server to access css files in the public folder
+app.use(express.static(path.join(__dirname, 'public')))
+
 // ROUTES
 const loginRoute = require('./routes/loginRoute');
+const registerRoute = require('./routes/registerRoute');
 
 app.use('/login', loginRoute);
+app.use('/register', registerRoute);
 
 app.get('/', middleware.requireLogin, (req, res, next) => {
 
