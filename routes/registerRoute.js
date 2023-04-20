5ecc9bc3edd8b8940b83b41927383c01b9e6d62f
Nathan G <Nathanglass26@gmail.com>
2023-04-20 12:24:12 +0100

    test inserting user into the db

--- routes/registerRoute.js
+++ routes/registerRoute.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 const bodyParser = require('body-parser');
+const User = require('../schemas/UserSchema');
 
 app.set('view engine', 'pug');
 app.set('views', 'views');
@@ -12,7 +13,7 @@ router.get('/', (req, res, next) => {
     res.status(200).render('register');
 });
 
-router.post('/', (req, res, next) => {
+router.post('/', async (req, res, next) => {
     var firstName = req.body.firstName.trim();
     var lastName = req.body.lastName.trim();
     var username = req.body.username.trim();
@@ -22,12 +23,42 @@ router.post('/', (req, res, next) => {
     var payload = req.body;
 
     if (firstName && lastName && username && email && password) {
-        
-    }
-    else {
-        payload.errorMessage = "Make sure each field is filled in correctly"
-        res.status(200).render('register', payload);
-    }
+        // check that the user exists
+        var user = await User.findOne({
+            $or: [
+                { username: username },
+                { email: email }
+            ]
+        })
+        .catch(error => {
+            console.log(error);
+            payload.errorMessage = "Something went wrong"
+            res.status(200).render('register', payload);
+        });
+
+        if (user == null) {
+            // no user found, insert it into the database
+            var data = req.body;
+            console.log(data);
+            User.create(data)
+            .then((user) => {
+                console.log(user);
+            })
+            .catch((error) => {
+                console.log(error);
+                payload.errorMessage = "Error creating user";
+                res.status(200).render('register', payload);
+            })
+        }
+        else if (user === user.email) {
+             // user found
+                payload.errorMessage = "Email already in use"
+            }
+            else {
+                payload.errorMessage = "Username already in use"
+            }
+            res.status(200).render('register', payload);
+        }
 });
 
 module.exports = router;
